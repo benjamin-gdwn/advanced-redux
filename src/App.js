59f3564e66d68ec07b8e5ce776966eafe243170e
Ben_G <ben.goodwin.work1@gmail.com>
2022-11-25 16:39:27 +0000

    async function added to app component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector} from 'react-redux'
-
+import { useEffect } from 'react';
 function App() {
 const cartState = useSelector(state => state.ui.cartIsVisible)
+const cart = useSelector(state=> state.cart);
 
+useEffect(() => {
+  fetch('https://react-http-realistic-be91c-default-rtdb.firebaseio.com/cart.json', {
+    method: 'PUT',
+    body: JSON.stringify(cart),
+  })
+}, [cart])
   return (
     <Layout>
       {cartState &&<Cart />}
